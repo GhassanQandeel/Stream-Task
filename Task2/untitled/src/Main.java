ea916403bc062a656629f177721f8c9bf653d7a4
GhassanQandeel <161489341+GhassanQandeel@users.noreply.github.com>
2024-08-07 11:20:48 +0300

    Second Task

--- Task2/untitled/src/Main.java
+++ Task2/untitled/src/Main.java
@@ -35,7 +35,10 @@ public class Main {
         cards.add(new Card("8","Rainy",new Date(2026-1900,8,24)));//true
         cards.add(new Card("9","Sunny",new Date(2025-1900,5,16)));
         cards.add(new Card("0","Sunny",new Date(2026-1900,4,28)));//true
-        cards.add(new Card("0","BBBBBB",new Date(2026-1900,4,28)));//true
+        cards.add(new Card("11","Sunny",new Date(2028-1900,5,29)));
+        cards.add(new Card("12","Sunny",new Date(2029-1900,4,28)));
+
+        cards.add(new Card("10","BBBBBB",new Date(2026-1900,4,28)));
 
          System.out.println("____________________________________________________________________________________________________\n");
                 cards.stream().forEach(card -> System.out.println(card.id+" "+card.fingerprint+" "+card.expires));
@@ -54,7 +57,7 @@ public class Main {
         }).forEach(card -> System.out.println(card.id+" "+card.fingerprint+" "+card.expires));
 
 */
-         cards
+         /*cards
                 .stream()
                 .collect(Collectors.groupingBy(card -> card.fingerprint))
                 .entrySet()
@@ -66,7 +69,23 @@ public class Main {
                     else
                         cards1.add(stringListEntry.getValue().get(1));
 
-                });
+                });*/
+
+
+        cards1 = (List<Card>) cards.stream()
+                .collect(Collectors.toMap(
+                        card -> card.fingerprint,
+                        card -> card,
+                        (card1, card2) -> card1.expires.after(card2.expires) ? card1 : card2
+                ))
+                .values()
+                .stream()
+                .collect(Collectors.toList());
+
+
+
+
+
           System.out.println("____________________________________________________________________________________________________\n");
           cards1.stream().forEach(card -> System.out.println(card.id+" "+card.fingerprint+" "+card.expires));
 
